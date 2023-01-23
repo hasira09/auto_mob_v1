ca1cfa6237fa0b11e7b7aa532da7437bd050105a
Hasira Mahel Koswatta <88621706+hasira09@users.noreply.github.com>
2023-01-23 17:55:07 +0530

    Delete auto_mob_v1 directory

--- auto_mob_v1/android/app/src/main/kotlin/com/example/auto_mob_v1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.auto_mob_v1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- auto_mob_v1/lib/global_users/reg_exp.dart
+++
@@ -1,1 +0,0 @@
-final RegExp emailRegex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
