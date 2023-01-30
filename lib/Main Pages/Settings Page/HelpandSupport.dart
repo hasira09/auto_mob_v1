24497eac3b96c96cebd029694f1f3c32c7a57817
Dinethri Gunawardhana <kavishkay@sltc.ac.lk>
2023-01-30 15:07:26 +0530

    Help and Support Page
    This page can get Phone Calls for help.

--- lib/Main Pages/Settings Page/HelpandSupport.dart
+++ lib/Main Pages/Settings Page/HelpandSupport.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+
+import 'Settings.dart';
 
 class help_support extends StatefulWidget {
   const help_support({Key? key}) : super(key: key);
@@ -11,6 +14,127 @@ class help_support extends StatefulWidget {
 class _help_supportState extends State<help_support> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final num2 = '+94 112100500';
+    final num3 = '+94 711100500';
+    final num4 = '+94 701082872';
+    final num5 ='+94 710744814';
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          "Help & Support",
+          style: TextStyle(color: Colors.black, fontSize: 22),
+        ),
+        leading: IconButton(
+          icon: Image.asset("assets/icons/back.png"),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => SettingsPage()));
+          },
+        ),
+        elevation: 0.0,
+      ),
+
+      body:  Center(
+        child: Column(
+          // add an elevation
+          children: [
+
+            SizedBox(
+              height: 25,
+            ),
+            Material(
+              child: Container(
+                child: Image.asset('assets/images/sltc.png', width: 320,),
+
+              ),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+            Material(
+              child: InkWell(
+                radius: 100,
+                onTap: ()async {
+                  await FlutterPhoneDirectCaller.callNumber(num2);
+                },
+                child: Image.asset('assets/images/s1.png', width: 320,),
+              ),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+            Material(
+              child: InkWell(
+                radius: 100,
+                onTap: () async {
+                  await FlutterPhoneDirectCaller.callNumber(num3);
+                },
+                child: Image.asset('assets/images/s2.png', width: 320,),
+
+              ),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+
+            Material(
+              child: InkWell(
+                radius: 100,
+                onTap: ()  {},
+                child: Image.asset('assets/images/s3.png', width: 320,),
+
+              ),
+            ),
+
+
+
+            SizedBox(
+              height: 25,
+            ),
+            Material(
+              child: InkWell(
+                radius: 100,
+                onTap: () {},
+                child: Image.asset('assets/images/auto.png', width: 320,),
+              ),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+
+            Material(
+              child: InkWell(
+                radius: 100,
+                onTap: () async {
+                  await FlutterPhoneDirectCaller.callNumber(num4);
+                },
+                child: Image.asset('assets/images/a1.png', width: 320,),
+
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+
+            Material(
+              child: InkWell(
+                radius: 100,
+                onTap: () async {
+                  await FlutterPhoneDirectCaller.callNumber(num5);
+                },
+                child: Image.asset('assets/images/a2.png', width: 320,),
+
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
